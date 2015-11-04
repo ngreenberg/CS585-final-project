8bf97b59628eb4edd1c5d103338d7910062deaed
pwneal <pneal@umass.edu>
2015-11-03 20:57:01 -0500

    Added featdelta, averaging

--- perceptron.py
+++ perceptron.py
@@ -14,12 +14,11 @@ def create_dataset(size=None):
 	"""
 	Reads in a set of texts and split into train and test sets, tagged by author
 	"""
+	do_train = 1
 	train, test = [],[]
 	for file_name in os.listdir(path)[:size]:
 		base_name = os.path.basename(file_name)
 		author = base_name.split('_',1)[0]
-		if author != 'Abraham Lincoln':
-			author = 'Not Abraham Lincoln'
 		if not author in authors:
 			authors.append(author)
 
@@ -27,10 +26,12 @@ def create_dataset(size=None):
 		with codecs.open(os.path.join(path, file_name),'r','utf8') as doc:
 			content = doc.read()
 			feat_vec = features.extract_features(content)
-			if random.randint(0,1):
+			if do_train:
 				train.append((feat_vec, author))
+				do_train = 0
 			else:
 				test.append((feat_vec, author))
+				do_train = 1
 	return train, test
 
 
@@ -39,20 +40,32 @@ def train_classifier(train, test, stepsize=1, numpasses=10):
 	Trains the classifier over a series of passes, and tests at each iteration
 	"""
 	weights = defaultdict(float)
+	weightSums = defaultdict(float)
+	avg_weights = defaultdict(float)
+
+	doc_count = 0
 	
 	for iteration in range(numpasses):
 		print "Training iteration %d" % iteration
 		random.shuffle(train)
 		for feat_vec, author in train:
-			pred_author = predict_author(feat_vec,weights)
+			doc_count+=1
+			pred_author = predict_author(feat_vec, weights)
 			if pred_author != author:
 				labeled_feat_vec = label_feat_vector(feat_vec, author)
-				for feat, weight in labeled_feat_vec.iteritems():
-					weights[feat]+=stepsize*weight
+				pred_feat_vec = label_feat_vector(feat_vec, pred_author)
+				feat_delta = dict_subtract(labeled_feat_vec, pred_feat_vec)
+				for feat, value in feat_delta.iteritems():
+					weights[feat]+=stepsize*value
+					weightSums[feat] += (doc_count-1) * stepsize * value
 
+		for feat in weightSums:
+			avg_weights[feat] = weights[feat] - 1/doc_count*weightSums[feat] 
 		print "Testing iteration %d" % iteration
-		test_classifier(test, weights)
-	return weights
+		
+		test_classifier(test, avg_weights)
+
+	return avg_weights
 
 def test_classifier(test, weights):
 	"""
@@ -71,7 +84,7 @@ def predict_author(feat_vec, weights):
 	"""
 	Finds the author with the highest score under the model
 	"""
-	scores = defaultdict(float)
+	scores = dict()
 	for author in authors:
 		author_feat_vec = label_feat_vector(feat_vec, author)
 		scores[author] = dict_dotprod(author_feat_vec, weights)
@@ -102,6 +115,13 @@ def dict_dotprod(d1, d2):
         total += d1.get(key,0) * d2.get(key,0)
     return total
 
+def dict_subtract(vec1, vec2):
+    """treat vec1 and vec2 as dict representations of sparse vectors"""
+    out = defaultdict(float)
+    out.update(vec1)
+    for k in vec2: out[k] -= vec2[k]
+    return dict(out)
+
 if __name__=='__main__':
-	training_set, testing_set = create_dataset(40)
+	training_set, testing_set = create_dataset(100)
 	train_classifier(training_set, testing_set)
